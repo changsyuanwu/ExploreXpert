48ef0d37b381387c32b9c55a0fa55c4d82c444b1
Richard Zhang <r468zhan@uwaterloo.ca>
2023-10-19 22:39:57 -0400

    Finish Google Maps fragment

--- app/src/main/java/com/example/explorexpert/MainActivity.kt
+++ app/src/main/java/com/example/explorexpert/MainActivity.kt
@@ -1,6 +1,14 @@
 package com.example.explorexpert
 
+import androidx.appcompat.app.AppCompatActivity;
 import android.os.Bundle
+
+import com.google.android.gms.maps.GoogleMap;
+import com.google.android.gms.maps.SupportMapFragment;
+import com.google.android.gms.maps.OnMapReadyCallback;
+import com.google.android.gms.maps.model.LatLng;
+import com.google.android.gms.maps.model.MarkerOptions;
+
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.compose.foundation.layout.fillMaxSize
@@ -12,35 +20,34 @@ import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
 import com.example.explorexpert.ui.theme.ExploreXpertTheme
 
-class MainActivity : ComponentActivity() {
+class MainActivity : AppCompatActivity(), OnMapReadyCallback {
     override fun onCreate(savedInstanceState: Bundle?) {
-        super.onCreate(savedInstanceState)
-        setContent {
-            ExploreXpertTheme {
-                // A surface container using the 'background' color from the theme
-                Surface(
-                    modifier = Modifier.fillMaxSize(),
-                    color = MaterialTheme.colorScheme.background
-                ) {
-                    Greeting("Android")
-                }
-            }
-        }
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        val mapFragment = supportFragmentManager.findFragmentById(R.id.map) as SupportMapFragment;
+        mapFragment.getMapAsync(this);
+    }
+
+    override fun onMapReady(googleMap: GoogleMap) {
+        googleMap.addMarker(
+            MarkerOptions().position(LatLng(0.0, 0.0)).title("Marker")
+        )
     }
 }
 
-@Composable
-fun Greeting(name: String, modifier: Modifier = Modifier) {
-    Text(
-        text = "Hello $name!",
-        modifier = modifier
-    )
-}
-
-@Preview(showBackground = true)
-@Composable
-fun GreetingPreview() {
-    ExploreXpertTheme {
-        Greeting("Android")
-    }
-}
+//@Composable
+//fun Greeting(name: String, modifier: Modifier = Modifier) {
+//    Text(
+//        text = "Hello $name!",
+//        modifier = modifier
+//    )
+//}
+//
+//@Preview(showBackground = true)
+//@Composable
+//fun GreetingPreview() {
+//    ExploreXpertTheme {
+//        Greeting("Android")
+//    }
+//}
