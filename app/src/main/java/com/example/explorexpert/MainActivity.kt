c98fc85774aec40c098a875b6fe44261db3d35c9
Richard Zhang <r468zhan@uwaterloo.ca>
2023-10-20 04:20:16 -0400

    Made new fragment for map for better integration with other tabs

--- app/src/main/java/com/example/explorexpert/MainActivity.kt
+++ app/src/main/java/com/example/explorexpert/MainActivity.kt
@@ -3,51 +3,20 @@ package com.example.explorexpert
 import androidx.appcompat.app.AppCompatActivity;
 import android.os.Bundle
 
-import com.google.android.gms.maps.GoogleMap;
-import com.google.android.gms.maps.SupportMapFragment;
-import com.google.android.gms.maps.OnMapReadyCallback;
-import com.google.android.gms.maps.model.LatLng;
-import com.google.android.gms.maps.model.MarkerOptions;
+import androidx.fragment.app.commit;
+import androidx.fragment.app.add;
 
-import androidx.activity.ComponentActivity
-import androidx.activity.compose.setContent
-import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.material3.MaterialTheme
-import androidx.compose.material3.Surface
-import androidx.compose.material3.Text
-import androidx.compose.runtime.Composable
-import androidx.compose.ui.Modifier
-import androidx.compose.ui.tooling.preview.Preview
-import com.example.explorexpert.ui.theme.ExploreXpertTheme
-
-class MainActivity : AppCompatActivity(), OnMapReadyCallback {
+class MainActivity : AppCompatActivity(R.layout.activity_main) {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        val manager = supportFragmentManager;
 
-        val mapFragment = supportFragmentManager.findFragmentById(R.id.map) as SupportMapFragment;
-        mapFragment.getMapAsync(this);
+        if (savedInstanceState == null) {
+            manager.commit {
+                setReorderingAllowed(true);
+                add<MapsFragment>(R.id.map_fragment);
+            }
+        }
     }
 
-    override fun onMapReady(googleMap: GoogleMap) {
-        googleMap.addMarker(
-            MarkerOptions().position(LatLng(0.0, 0.0)).title("Marker")
-        )
-    }
 }
-
-//@Composable
-//fun Greeting(name: String, modifier: Modifier = Modifier) {
-//    Text(
-//        text = "Hello $name!",
-//        modifier = modifier
-//    )
-//}
-//
-//@Preview(showBackground = true)
-//@Composable
-//fun GreetingPreview() {
-//    ExploreXpertTheme {
-//        Greeting("Android")
-//    }
-//}
