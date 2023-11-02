4ba7f1822387ee1e219c14f436f7966f2eaea1bb
Yi Bo Cheng <ybcheng@uwaterloo.ca>
2023-11-02 15:18:18 -0400

    update calendarFragment with selected date view + ListView for events

--- app/src/main/java/com/example/explorexpert/ui/view/CalendarFragment.kt
+++ app/src/main/java/com/example/explorexpert/ui/view/CalendarFragment.kt
@@ -1,8 +1,40 @@
 package com.example.explorexpert.ui.view
 
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.CalendarView
+import android.widget.TextView
 import androidx.fragment.app.Fragment
 import com.example.explorexpert.R
 
-class CalendarFragment : Fragment(R.layout.fragment_calendar) {
+
+class CalendarFragment : Fragment() {
+
+    lateinit var dateTV: TextView
+    lateinit var calendarView: CalendarView
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_calendar, container, false)
+    }
+
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+        calendarView = view.findViewById(R.id.calendarView) as CalendarView
+        dateTV = view.findViewById(R.id.dateView) as TextView
+
+        calendarView.setOnDateChangeListener(CalendarView.OnDateChangeListener { view, year, month, dayOfMonth ->
+                    val Date = (dayOfMonth.toString() + "-"
+                            + (month + 1) + "-" + year)
+                    // set this date in TextView for Display
+                    dateTV.setText(Date)
+                })
+    }
 
 }
