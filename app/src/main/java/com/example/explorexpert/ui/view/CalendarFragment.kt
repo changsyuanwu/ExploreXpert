a147d70e154e7643e07db877ffe6f0042c8c5209
Yi Bo Cheng <ybcheng@uwaterloo.ca>
2023-11-02 20:34:49 -0400

    removed popup, put insert bar in the same fragment instead + Inserting to lists in different dates is working

--- app/src/main/java/com/example/explorexpert/ui/view/CalendarFragment.kt
+++ app/src/main/java/com/example/explorexpert/ui/view/CalendarFragment.kt
@@ -5,20 +5,23 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import android.widget.ArrayAdapter
+import android.widget.Button
 import android.widget.CalendarView
+import android.widget.EditText
 import android.widget.ListView
 import android.widget.TextView
-import androidx.appcompat.app.AppCompatActivity
+import android.widget.Toast
 import androidx.fragment.app.Fragment
 import com.example.explorexpert.R
-import com.google.android.material.floatingactionbutton.FloatingActionButton
+import java.time.LocalDateTime
+import java.time.format.DateTimeFormatter
 
 
 class CalendarFragment : Fragment() {
 
     lateinit var dateTV: TextView
     lateinit var calendarView: CalendarView
-    lateinit var addButton: FloatingActionButton
+    lateinit var addBtn: Button
     lateinit var eventListView : ListView
     val allCalendarEvents = mutableMapOf<String, ArrayList<String>>()
 
@@ -40,32 +43,53 @@ class CalendarFragment : Fragment() {
         dateTV = view.findViewById(R.id.dateView) as TextView
         eventListView = view.findViewById(R.id.eventListView) as ListView
 
-        var context_temp = context
-        //var listAdapter = ArrayAdapter<String>(context, android.R.layout.simple_list_item_1)
 
+        //set current date as default
+        var selectedDate : String
+        val current = LocalDateTime.now()
+        val formatter = DateTimeFormatter.ofPattern("yyyy-M-d")
+        selectedDate = current.format(formatter)
+        dateTV.setText(selectedDate)
+
+        // changing dates (and lists) by clicking dates on calendar
         calendarView.setOnDateChangeListener{
-            view, year, month, dayOfMonth -> val Date = (dayOfMonth.toString() + "-" + (month + 1) + "-" + year)
-            dateTV.setText(Date)
-
-
+            view, year, month, dayOfMonth -> selectedDate = (year.toString() + "-" + (month + 1) + "-" + dayOfMonth)
+            dateTV.setText(selectedDate)
 
             // load list by date selected
-            //val adapter = MapAdapter(this, android.R.layout.simple_list_item_1, allCalendarEvents)
-            if (allCalendarEvents[Date] != null) {
-                var myList: ArrayList<String> = arrayListOf()
-                myList = allCalendarEvents[Date]!!
-                var listAdapter = context_temp?.let { ArrayAdapter<String>(it, android.R.layout.simple_list_item_1, myList) }
-                eventListView.setAdapter(listAdapter)
+            var currentList = allCalendarEvents[selectedDate]
+            if (currentList == null) {
+                // no list, load an empty list
+                val emptyList: ArrayList<String> = arrayListOf()
+                currentList = emptyList
             }
-
+            val listAdapter = context?.let { ArrayAdapter(it, android.R.layout.simple_list_item_1, currentList!!) }
+            eventListView.setAdapter(listAdapter)
         }
 
+        // add events to list
+        addBtn = view.findViewById(R.id.btnEventAdd)
+        val editText = view.findViewById<EditText>(R.id.et_eventInput)
+        addBtn.setOnClickListener{
+            val value = editText.text.toString()
 
-        addButton = view.findViewById(R.id.addEventButton) as FloatingActionButton
-        addButton.setOnClickListener{
-            val showPopup = PopUpFragment()
-            showPopup.show((activity as AppCompatActivity).supportFragmentManager, "showPopUp")
+            if (value.isEmpty()) {
+                Toast.makeText(context,"Please fill out the blank", Toast.LENGTH_LONG).show()
+            } else {
+                var currentList: ArrayList<String> = arrayListOf()
+                if (allCalendarEvents[selectedDate] == null) {
+                    currentList.add(value)
+                    allCalendarEvents[selectedDate] = currentList
+                } else {
+                    allCalendarEvents[selectedDate]?.add(value)
+                    currentList = allCalendarEvents[selectedDate]!!
+                }
+                val listAdapter = context?.let { ArrayAdapter(it, android.R.layout.simple_list_item_1, currentList) }
+                eventListView.setAdapter(listAdapter)
+                editText.setText(null)
+            }
         }
+
     }
 
 }
