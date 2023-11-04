1e3a67f74f2d03db62654fd885fbfb91c1b2c6ca
Chang-Syuan Wu <william.wu1219@gmail.com>
2023-11-04 18:45:06 -0400

    grab user name on home screen

--- app/src/main/java/com/example/explorexpert/ui/view/HomeFragment.kt
+++ app/src/main/java/com/example/explorexpert/ui/view/HomeFragment.kt
@@ -5,6 +5,7 @@ import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.TextView
 import androidx.fragment.app.Fragment
 import com.example.explorexpert.R
 import com.example.explorexpert.SplashScreenActivity
@@ -12,6 +13,9 @@ import com.example.explorexpert.databinding.FragmentHomeBinding
 import com.example.explorexpert.ui.viewmodel.HomeViewModel
 import com.google.android.material.shape.MaterialShapeDrawable
 import dagger.hilt.android.AndroidEntryPoint
+import kotlinx.coroutines.CoroutineScope
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.launch
 import javax.inject.Inject
 
 @AndroidEntryPoint
@@ -27,7 +31,7 @@ class HomeFragment : Fragment() {
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
-    ): View? {
+    ): View {
         _binding = FragmentHomeBinding.inflate(inflater, container, false)
         return binding.root
     }
@@ -35,17 +39,36 @@ class HomeFragment : Fragment() {
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
 
-        binding.appBarLayout.statusBarForeground = MaterialShapeDrawable.createWithElevationOverlay(context)
+        binding.appBarLayout.statusBarForeground =
+            MaterialShapeDrawable.createWithElevationOverlay(context)
 
         configureButtons()
         configureNavSideBar()
+        configureUserDetails()
+    }
+
+    private fun configureUserDetails() {
+        CoroutineScope(Dispatchers.Main).launch {
+            val currentUserName = homeViewModel.getCurrentUserName()
+            if (currentUserName != "") {
+                binding.txtName.text = currentUserName
+                binding.navigationViewSideBar.findViewById<TextView>(R.id.txtNameSideNav).text =
+                    currentUserName
+            }
+
+            val currentUserEmail = homeViewModel.getCurrentUserEmail()
+            if (currentUserEmail != "") {
+                binding.navigationViewSideBar.findViewById<TextView>(R.id.txtEmailSideNav).text =
+                    currentUserEmail
+            }
+        }
     }
 
     private fun configureNavSideBar() {
         binding.navigationViewSideBar.setNavigationItemSelectedListener { menuItem ->
             when (menuItem.itemId) {
                 R.id.btnLogout -> logOut()
-
+                // Add other buttons for side nav bar here
                 else -> {
                     return@setNavigationItemSelectedListener false
                 }
