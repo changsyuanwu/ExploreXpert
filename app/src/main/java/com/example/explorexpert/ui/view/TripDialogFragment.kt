9491cfb9b19452edb89f643c312540344b66e4be
Chang-Syuan Wu <william.wu1219@gmail.com>
2023-11-15 23:15:44 -0500

    trip dialog opens in full screen!

--- app/src/main/java/com/example/explorexpert/ui/view/TripDialogFragment.kt
+++ app/src/main/java/com/example/explorexpert/ui/view/TripDialogFragment.kt
@@ -1,10 +1,66 @@
 package com.example.explorexpert.ui.view
 
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
 import androidx.fragment.app.DialogFragment
+import com.example.explorexpert.R
+import com.example.explorexpert.data.model.Trip
+import com.example.explorexpert.databinding.DialogTripBinding
+import com.example.explorexpert.databinding.FragmentPlanBinding
+import com.example.explorexpert.ui.viewmodel.CreateTripViewModel
+import com.example.explorexpert.ui.viewmodel.TripViewModel
 import dagger.hilt.android.AndroidEntryPoint
+import javax.inject.Inject
 
 @AndroidEntryPoint
-class TripDialogFragment(): DialogFragment() {
+class TripDialogFragment(
+    private val trip: Trip
+) : DialogFragment() {
 
+    @Inject
+    lateinit var tripViewModel: TripViewModel
 
+    private var _binding: DialogTripBinding? = null
+
+    private val binding get() = _binding!!
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setStyle(STYLE_NO_FRAME, R.style.FullScreenDialogStyle)
+    }
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View {
+        // Inflate the layout for this fragment
+        _binding = DialogTripBinding.inflate(inflater, container, false)
+        return binding.root
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+        showProgressIndicator()
+
+//        configureRecyclerView()
+        configureButtons()
+//        configureObservers()
+    }
+
+    private fun configureButtons() {
+        binding.btnBackIcon.setOnClickListener {
+            this.dismiss()
+        }
+    }
+
+    private fun showProgressIndicator() {
+        binding.progressIndicator.visibility = View.VISIBLE
+    }
+
+    private fun hideProgressIndicator() {
+        binding.progressIndicator.visibility = View.INVISIBLE
+    }
 }
