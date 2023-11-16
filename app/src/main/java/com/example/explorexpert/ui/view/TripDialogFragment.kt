9960d9c5391227e311c3b6008e120bd49487cc25
changsyuanwu <william.wu1219@gmail.com>
2023-11-15 22:00:03 -0500

    add basic trip dialog fragment

---
+++ app/src/main/java/com/example/explorexpert/ui/view/TripDialogFragment.kt
@@ -0,0 +1,10 @@
+package com.example.explorexpert.ui.view
+
+import androidx.fragment.app.DialogFragment
+import dagger.hilt.android.AndroidEntryPoint
+
+@AndroidEntryPoint
+class TripDialogFragment(): DialogFragment() {
+
+
+}
