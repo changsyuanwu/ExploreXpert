724d4b6364be6d9c55645dc6870260394ee9357f
Chang-Syuan Wu <william.wu1219@gmail.com>
2023-11-30 18:19:06 -0500

    add necessary libraries for nearby places search

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -75,8 +75,8 @@ dependencies {
     implementation("androidx.compose.material3:material3")
     implementation("com.google.android.gms:play-services-location:21.0.1")
     implementation("com.google.android.gms:play-services-maps:18.1.0")
-    implementation ("com.google.android.gms:play-services-places:16.0.0")
-    implementation ("com.google.android.libraries.places:places:3.2.0")
+    implementation("com.google.android.gms:play-services-places:16.0.0")
+    implementation("com.google.android.libraries.places:places:3.2.0")
     implementation("androidx.fragment:fragment-ktx:1.6.1")
     implementation("androidx.appcompat:appcompat:1.6.1")
     implementation("androidx.annotation:annotation:1.6.0")
@@ -132,15 +132,25 @@ dependencies {
     implementation("com.squareup.retrofit2:retrofit:2.9.0")
     implementation("com.squareup.okhttp3:okhttp:5.0.0-alpha.2")
     implementation("com.squareup.retrofit2:converter-gson:2.9.0")
+    implementation("com.squareup.retrofit2:converter-moshi:2.9.0")
+    implementation("com.jakewharton.retrofit:retrofit2-kotlin-coroutines-adapter:0.9.2")
 
     // Circular image view
     implementation("de.hdodenhof:circleimageview:3.1.0")
 
     // Google place search
     implementation("com.urmich.android:placesearch-ktx:1.1.0")
+
+    // Moshi
+    implementation("com.squareup.moshi:moshi:1.13.0")
+    implementation("com.squareup.moshi:moshi-kotlin:1.13.0")
 }
 
 kapt {
     correctErrorTypes = true
     generateStubs = true
+}
+
+secrets {
+    defaultPropertiesFileName = "local.properties"
 }
