e0769e308eae2fe38395eb3dc3540c55eeb5fdc4
Yi Bo Cheng <ybcheng@uwaterloo.ca>
2023-12-03 23:28:53 -0500

    delete calendar event support + delete trip events removes it from calendar (does not delete trip)

--- app/src/main/java/com/example/explorexpert/adapters/EventAdapter.kt
+++ app/src/main/java/com/example/explorexpert/adapters/EventAdapter.kt
@@ -1,42 +1,65 @@
 package com.example.explorexpert.adapters
 
-import com.example.explorexpert.R
+import android.util.Log
 import android.view.LayoutInflater
-import android.view.View
 import android.view.ViewGroup
-import android.widget.TextView
+import androidx.recyclerview.widget.ListAdapter
+import androidx.recyclerview.widget.DiffUtil
 import androidx.recyclerview.widget.RecyclerView
 import com.example.explorexpert.data.model.Event
+import com.example.explorexpert.databinding.EventItemBinding
+import com.example.explorexpert.ui.viewmodel.CalendarViewModel
 
+import javax.inject.Inject
 
 class EventAdapter(
-    private val eventItems: List<Event>,
-) : RecyclerView.Adapter<EventAdapter.ViewHolder>() {
+    private val itemClickListener: ItemClickListener
+) : ListAdapter<Event, EventAdapter.ViewHolder>(DiffCallback()){
 
-    class ViewHolder(itemView: View) : RecyclerView.ViewHolder(itemView) {
-        val eventTextView: TextView = itemView.findViewById(R.id.tvEvent)
-        val eventDateTextView: TextView = itemView.findViewById(R.id.tvEventDate)
+    @Inject
+    lateinit var calendarViewModel: CalendarViewModel
+    inner class ViewHolder(private val binding: EventItemBinding) : RecyclerView.ViewHolder(binding.root) {
+        fun bind(event: Event) {
+            binding.tvEvent.text = event.name
+
+            if (event.startDate == event.endDate) {
+                binding.tvEventDate.text = event.startDate
+            } else {
+                val dateRange = event.startDate + " - " + event.endDate
+                binding.tvEventDate.text = dateRange
+            }
+
+            binding.btnDelEvent.setOnClickListener {
+                itemClickListener.onItemClick(event)
+            }
+        }
+
+
     }
     override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ViewHolder {
-        val view = LayoutInflater.from(parent.context).inflate(R.layout.event_item, parent, false)
-        return ViewHolder(view)
+        val inflater = LayoutInflater.from(parent.context)
+        val binding = EventItemBinding.inflate(inflater, parent, false)
+        return ViewHolder(binding)
     }
 
     override fun onBindViewHolder(holder: ViewHolder, position: Int) {
-        val item = eventItems[position]
-        holder.eventTextView.text = item.name
+        val event = getItem(position)
 
-        if (item.startDate == item.endDate) {
-            holder.eventDateTextView.text = item.startDate
-        } else {
-            val dateRange = item.startDate + " - " + item.endDate
-            holder.eventDateTextView.text = dateRange
-        }
-
+        holder.bind(event)
     }
 
-    override fun getItemCount(): Int {
-        return eventItems.size
+    interface ItemClickListener {
+        fun onItemClick(event: Event)
+    }
+
+    private class DiffCallback : DiffUtil.ItemCallback<Event>() {
+        override fun areItemsTheSame(oldItem: Event, newItem: Event): Boolean {
+            return oldItem.id == newItem.id
+        }
+
+        override fun areContentsTheSame(oldItem: Event, newItem: Event): Boolean {
+            return oldItem == newItem
+        }
     }
 
 }
