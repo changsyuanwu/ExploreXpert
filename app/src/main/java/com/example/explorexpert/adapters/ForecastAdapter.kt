932fa94fc045b4bcd65b9da6b27c03ea05581ddf
Mick <m479wang@uwaterloo.ca>
2023-12-05 01:10:22 -0500

    fix UI for weather fragment

--- app/src/main/java/com/example/explorexpert/adapters/ForecastAdapter.kt
+++ app/src/main/java/com/example/explorexpert/adapters/ForecastAdapter.kt
@@ -1,13 +1,24 @@
 package com.example.explorexpert.adapters
+
+import android.content.Context
+import android.graphics.drawable.Drawable
+import android.util.Log
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.ImageView
 import android.widget.TextView
+import androidx.core.content.ContentProviderCompat.requireContext
+import androidx.core.content.ContextCompat
+import androidx.recyclerview.widget.DividerItemDecoration
 import androidx.recyclerview.widget.RecyclerView
 import com.example.explorexpert.R
+import java.io.IOException
+import java.io.InputStream
 
-class ForecastAdapter(private val forecastItems: ArrayList<ForecastItem>) :
+class ForecastAdapter(private val forecastItems: ArrayList<ForecastItem>, private val context: Context) :
     RecyclerView.Adapter<ForecastAdapter.ViewHolder>() {
+
 
     override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ViewHolder {
         val view = LayoutInflater.from(parent.context).inflate(R.layout.weather_item, parent, false)
@@ -26,15 +37,49 @@ class ForecastAdapter(private val forecastItems: ArrayList<ForecastItem>) :
     inner class ViewHolder(itemView: View) : RecyclerView.ViewHolder(itemView) {
         private val dateTextView: TextView = itemView.findViewById(R.id.dateTextView)
         private val temperatureTextView: TextView = itemView.findViewById(R.id.temperatureTextView)
-        private val weatherConditionTextView: TextView = itemView.findViewById(R.id.weatherConditionTextView)
+        private val weatherConditionTextView: TextView =
+            itemView.findViewById(R.id.weatherConditionTextView)
+        private val weatherIconImageView: ImageView = itemView.findViewById(R.id.weatherIconImageView)
+        val lineView = itemView.findViewById<View>(R.id.divider)
 
         fun bind(item: ForecastItem) {
             // Bind data to views
             dateTextView.text = item.date
             temperatureTextView.text = item.temperature
             weatherConditionTextView.text = item.weatherCondition
+            loadWeatherIcon(weatherIconImageView, item.weatherCondition)
+            if (position == forecastItems.size - 1) {
+                lineView.visibility = View.GONE
+            }
+        }
+    }
+    private fun loadWeatherIcon(imageView: ImageView, weatherStatus: String) {
+        Log.i("TAG", "Weather status received: '$weatherStatus'")
+        val imageName: String = when (weatherStatus) {
+            "Thunderstorm" -> "Thundery.png"
+            "Clouds" -> "Cloudy.png"
+            "Rain" -> "Rainy.png"
+            "Snow" -> "Snowy.png"
+            // Add more cases as needed for other weather statuses
+            else -> "Sunny.png"
+        }
+        try {
+            // Load the image from the assets folder
+            val inputStream: InputStream = context.assets.open(imageName)
+            val drawable = Drawable.createFromStream(inputStream, null)
+            imageView.setImageDrawable(drawable)
+        } catch (e: IOException) {
+            e.printStackTrace()
         }
     }
 
-    data class ForecastItem(val date: String, val temperature: String, val weatherCondition: String)
+
+    data class ForecastItem(
+        val date: String,
+        val temperature: String,
+        val weatherCondition: String,
+        val weatherIcon: String
+    )
+
+
 }
--- app/src/main/java/com/example/explorexpert/ui/view/HistoricalWeatherFragment.kt
+++ app/src/main/java/com/example/explorexpert/ui/view/HistoricalWeatherFragment.kt
@@ -1,3 +1,4 @@
+import android.app.Activity
 import android.content.Context
 import android.content.pm.ApplicationInfo
 import android.content.pm.PackageManager
@@ -10,7 +11,6 @@ import android.view.ViewGroup
 import android.widget.LinearLayout
 import android.widget.TextView
 import androidx.fragment.app.Fragment
-import com.android.volley.Request
 import com.android.volley.RequestQueue
 import com.android.volley.Response
 import com.android.volley.toolbox.StringRequest
@@ -21,17 +21,42 @@ import org.json.JSONObject
 import java.util.Locale
 import java.util.Calendar
 import android.icu.util.TimeZone
+import android.view.Gravity
+import android.widget.Button
+import android.widget.ImageView
+import androidx.activity.result.ActivityResult
+import androidx.activity.result.contract.ActivityResultContracts
+import androidx.compose.ui.graphics.Color
+import androidx.core.content.ContextCompat
+import com.example.explorexpert.ui.view.TAG
+import com.google.android.libraries.places.api.model.Place
+import com.google.android.libraries.places.widget.Autocomplete
+import com.google.android.libraries.places.widget.AutocompleteActivity
+import com.google.android.libraries.places.widget.model.AutocompleteActivityMode
 import java.util.Date
 
 class HistoricalWeatherFragment : Fragment() {
     private lateinit var weatherEntriesLayout: LinearLayout
     private lateinit var appInfo: ApplicationInfo
+    private lateinit var addressTextView: TextView
+    private var latitude  = 43.4723
+    private var longitude = -80.5449
 
     override fun onAttach(context: Context) {
         super.onAttach(context)
         appInfo = requireContext().packageManager
             .getApplicationInfo(requireContext().packageName, PackageManager.GET_META_DATA)
     }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+        val backArrow: ImageView = view.findViewById(R.id.backArrow)
+        backArrow.setOnClickListener {
+            requireActivity().supportFragmentManager.popBackStack()
+        }
+    }
+
     override fun onCreateView(
         inflater: LayoutInflater,
         container: ViewGroup?,
@@ -39,12 +64,16 @@ class HistoricalWeatherFragment : Fragment() {
     ): View? {
         val view = inflater.inflate(R.layout.fragment_historical_weather, container, false)
         weatherEntriesLayout = view.findViewById(R.id.weatherEntriesLayout)
+        addressTextView = view.findViewById(R.id.address)
 
         // Retrieve latitude, longitude, start date, and end date from arguments
-        val latitude = arguments?.getDouble("latitude", 0.0) ?: 0.0
-        val longitude = arguments?.getDouble("longitude", 0.0) ?: 0.0
+        latitude = arguments?.getDouble("latitude", 0.0) ?: 0.0
+        longitude = arguments?.getDouble("longitude", 0.0) ?: 0.0
         val startDate = convertDateToUnix(arguments?.getString("startDate", "") ?: "")
         val endDate = convertDateToUnix(arguments?.getString("endDate", "") ?: "")
+
+        val address = arguments?.getString("address")
+        addressTextView.text = address
 
         // Fetch historical weather data
         fetchHistoricalWeatherData(latitude, longitude, startDate, endDate)
@@ -57,6 +86,7 @@ class HistoricalWeatherFragment : Fragment() {
         val historicalWeatherUrl =
             "https://history.openweathermap.org/data/2.5/history/city?lat=$latitude&lon=$longitude&start=$startDate&end=$endDate&units=metric&appid=$apiKey"
 
+        weatherEntriesLayout.removeAllViews()
         val queue: RequestQueue = Volley.newRequestQueue(requireContext())
         // Create a GET request
         val stringRequest = object : StringRequest(
@@ -71,12 +101,25 @@ class HistoricalWeatherFragment : Fragment() {
                 }
             },
             Response.ErrorListener { error ->
+                if (error.networkResponse != null) {
+                    // If the HTTP error code is 400, handle the error message from the JSON response
+                    val errorResponse = String(error.networkResponse.data)
+                    try {
+                        val errorJson = JSONObject(errorResponse)
+                        val errorCode = errorJson.optInt("code")
+                        val errorMessage = errorJson.optString("message")
+                        if (errorCode == 400000) {
+                            displayErrorMessage(errorMessage+": maximum 1 year back")
+                        }
+                    } catch (exc: JSONException) {
+                        Log.e("JSON Parsing Error", "Error: ${exc.message}")
+                    }
+                }
             }) {
         }
 
         queue.add(stringRequest)
     }
-
     private fun displayHistoricalWeatherData(jsonResponse: JSONObject) {
         try {
             val weatherData = jsonResponse.getJSONArray("list")
@@ -95,7 +138,6 @@ class HistoricalWeatherFragment : Fragment() {
 
                 val formattedDate = convertUnixToDate(timestamp)
 
-                // Check if the date already exists in the set before creating the entry
                 if (!uniqueDates.contains(formattedDate)) {
                     createWeatherEntry(formattedDate, temperatureAvg, condition)
                     // Add the date to the set to avoid duplicates
@@ -106,7 +148,6 @@ class HistoricalWeatherFragment : Fragment() {
             Log.e("JSON Parsing Error", "Error: ${exc.message}")
         }
     }
-
     private fun convertUnixToDate(unixTimestamp: Long): String {
         val dateFormat = SimpleDateFormat("MMM dd, yyyy", Locale.getDefault())
         val date = Date(unixTimestamp * 1000)
@@ -125,12 +166,34 @@ class HistoricalWeatherFragment : Fragment() {
 
         weatherEntriesLayout.addView(entryView)
     }
+
     private fun convertDateToUnix(dateString: String): Long {
         val dateFormat = SimpleDateFormat("yyyy-MM-dd", Locale.getDefault())
         dateFormat.timeZone = TimeZone.getTimeZone("UTC")
         val parsedDate = dateFormat.parse(dateString)
         val unixTimestamp = parsedDate?.time ?: 0
         return unixTimestamp / 1000
+    }
+
+    private fun displayErrorMessage(errorMessage: String) {
+        val context = requireContext()
+
+        // Create a TextView for the error message
+        val errorTextView = TextView(context)
+        errorTextView.text = errorMessage
+
+        // Set the warning style for the error message
+        errorTextView.setTextColor(ContextCompat.getColor(context, android.R.color.white))
+        errorTextView.setBackgroundResource(R.drawable.warning_background)
+        errorTextView.setPadding(16, 8, 16, 8)
+        errorTextView.gravity = Gravity.CENTER // Center text in the TextView
+
+        // Set the text size
+        val textSize = 20f
+        errorTextView.textSize = textSize // Set the text size
+
+        // Add the TextView to the layout
+        weatherEntriesLayout.addView(errorTextView)
     }
 
 }
